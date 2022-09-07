bd221eb42eab0fd6ed57ba05266b1991f5412736
Валентина <abc99-33@yandex.ru>
2022-09-07 21:15:19 +0300

    68

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -7,8 +7,8 @@ int Akkerman (int m, int n)
 {
     if (n == 0 && m == 0) return 1;
     if (m == 0) return n + 1;
-    if (m>0 && n == 0) return Akkerman(m - 1 , 1);
-    return Akkerman(m-1, Akkerman(m, n-1));
+    if (m > 0 && n == 0) return Akkerman(m - 1 , 1);
+    return Akkerman(m - 1, Akkerman(m, n - 1));
 }
 
 Console.WriteLine(Akkerman(3,2));
